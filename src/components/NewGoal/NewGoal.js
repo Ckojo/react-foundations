0d48aee07ebe862576b82bc47fa73f6ee10aa5ec
Jovan Sremacki <jovansremacki@gmail.com>
2023-02-09 21:08:30 +0100

    New Goal modifications

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React,  { useState } from 'react';
 import './NewGoal.css';
 
 const NewGoal = props => {
+    const [userInput, setUserInput] = useState('');
+
     const addGoalHandler = e => {
         e.preventDefault();
         
         const newGoal = { 
             id: Math.random().toString(),
-            text: 'My new goal'
+            text: userInput
         };
+
+        setUserInput('');
 
         props.onAddGoal(newGoal);
     }
 
+    const userInputHandler = e => {
+        setUserInput(e.target.value);
+    }
+
     return (
         <form className='new-goal' onSubmit={addGoalHandler}>
-            <input type='text' />
+            <input type='text' value={userInput} onChange={userInputHandler} />
             <button type='submit'>Add Goal</button>
         </form>
     );
